0ae5dd354e1fcbb2c8a0d54689fd3bcfeafe8fd1
lexashvetsoff <lexa.shvetsoff@yandex.ru>
2022-04-16 23:04:47 +0800

    setup admin UI for likes

--- property/models.py
+++ property/models.py
@@ -48,13 +48,22 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField('Новостройка', db_index=None, null=True)
+    liked_by = models.ManyToManyField(
+        User,
+        related_name="liked_flat",
+        verbose_name='Кто лайкнул'
+    )
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
 
 
 class Complaint(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Кто жаловался'
+    )
     flat = models.ForeignKey(
         Flat,
         on_delete=models.CASCADE,

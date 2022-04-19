b25f90da78a66a548529e0b179a72155db908c17
lexashvetsoff <lexa.shvetsoff@yandex.ru>
2022-04-19 21:35:48 +0800

    added owner model

--- property/models.py
+++ property/models.py
@@ -73,3 +73,12 @@ class Complaint(models.Model):
         verbose_name='Квартира, на которую пожаловались'
     )
     text = models.TextField('Текст жалобы')
+
+
+class Owner(models.Model):
+    owner = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+    flats = models.ManyToManyField(Flat,
+    related_name='owner_flats',
+    verbose_name='Квартиры в собственности')
